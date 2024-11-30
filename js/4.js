081991ae46ee05c583e328af660b1b7f75714b93
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-11-30 14:04:16 +0500

    ahsvdbc

--- js/4.js
+++ js/4.js
@@ -1,10 +1,10 @@
-// let str = "abc"
-// res=" "
-// for(let i = 0 ; i < str.length; i++){
-//     res += (str.charCodeAt(i))+" "
+let str = "abc"
+res=" "
+for(let i = 0 ; i < str.length; i++){
+    res += (str.charCodeAt(i) + 1)+" "
 
-// }
+}
 // console.log(str.charCodeAt(+1))
-// console.log(str.charCodeAt(+2))
 // console.log(str.charCodeAt())
-// console.log(str.charCodeAt(+1) + res)
+console.log(res)
+// console.log(str.charCodeAt(i) + 1)
