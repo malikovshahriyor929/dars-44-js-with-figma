bc7294f9fa5a49582278267ade21d4b4662557c9
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-11-30 14:05:02 +0500

    sdv

--- js/4.js
+++ js/4.js
@@ -1,10 +1,14 @@
-let str = "abc"
-res=" "
-for(let i = 0 ; i < str.length; i++){
-    res += (str.charCodeAt(i) + 1)+" "
+// let str = "abc"
+// res=" "
+// for(let i = 0 ; i < str.length; i++){
+//     res += (str.charCodeAt(i) + 1)+" "
 
-}
-// console.log(str.charCodeAt(+1))
-// console.log(str.charCodeAt())
-console.log(res)
+// }
+
+// console.log(res)
+
+
+
 // console.log(str.charCodeAt(i) + 1)
+// // console.log(str.charCodeAt(+1))
+// // console.log(str.charCodeAt())
